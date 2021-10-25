ba2ac49b474bbc4f40efd3c05ef0419965dea69a
Bárbara Nassar <ba.almeida@hotmail.com>
2021-10-25 20:51:11 +0200

    #9: introducing footer and making it work for mobile and desktop

---
+++ src/components/footer/footer.js
@@ -0,0 +1,24 @@
+import { Divide } from 'hamburger-react';
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div class="ft-item">
+                <div class="ft-item-company">
+                    Teste 1
+                </div>
+
+                <div class="ft-item-links">
+                    Teste 2
+                </div>
+
+                <div class="ft-item-links">
+                    Teste 3
+                </div>
+            </div>
+        </footer>
+    );
+  }
+
+export default Footer;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,13 @@
 import Header from '../components/header/header';
 import Main from '../components/main/main';
+import Footer from '../components/footer/footer';
 
 function Home() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
