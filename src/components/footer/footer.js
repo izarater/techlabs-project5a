32a1bbfb32121605421685e9c963ee5ad8322ec0
Bárbara Nassar <ba.almeida@hotmail.com>
2021-10-26 03:34:35 +0200

    #09: Create footer content

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,20 +1,40 @@
 import { Divide } from 'hamburger-react';
 import './footer.css';
+import facebook from '../../assets/facebook.png'
+import instagram from '../../assets/instagram.png'
+import twitter from '../../assets/twitter.png'
 
 function Footer() {
     return (
         <footer className="footer">
             <div class="ft-item">
                 <div class="ft-item-company">
-                    Teste 1
+                    <h2>SaveFood</h2>
+                    <p class="footer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, metus id fermentum ultrices, quam urna luctus felis, quis malesuada lorem ex ut metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, metus id fermentum ultrices, quam urna luctus felis, quis malesuada lorem ex ut metus.</p>
+                    
+                    <div class="ft-item-company-social">
+                        <a href="http://facebook.com"><img src={facebook} alt='facebook logo' /></a>
+                        <a href="https://instagram.com"><img src={instagram} alt='instagram logo' /></a>
+                        <a href="https://twitter.com"><img src={twitter} alt='twitter logo' /></a>
+                    </div>
                 </div>
 
                 <div class="ft-item-links">
-                    Teste 2
+                    <h3>Sitemap</h3>
+                    <ul>
+                        <li><a href="/index" alt="home">Home</a></li>
+                        <li>About us</li>
+                        <li>Register your restaurant</li>
+                    </ul>
                 </div>
 
                 <div class="ft-item-links">
-                    Teste 3
+                    <h3>More information</h3>
+                    <ul>
+                        <li>Look all cities</li>
+                        <li>Read more in our FAQ</li>
+                        <li>How you can help me us</li>
+                    </ul>
                 </div>
             </div>
         </footer>
