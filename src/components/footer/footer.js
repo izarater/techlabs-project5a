e0790f3d55c3a01b1a9ba729e6ab5902cbb9c7e9
Bárbara Nassar <ba.almeida@hotmail.com>
2021-10-26 16:03:39 +0200

    #9: Fix text align center in mobile

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,35 +7,31 @@ import twitter from '../../assets/twitter.png'
 function Footer() {
     return (
         <footer className="footer">
-            <div class="ft-item">
-                <div class="ft-item-company">
-                    <h2>SaveFood</h2>
-                    <p class="footer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, metus id fermentum ultrices, quam urna luctus felis, quis malesuada lorem ex ut metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, metus id fermentum ultrices, quam urna luctus felis, quis malesuada lorem ex ut metus.</p>
-                    
-                    <div class="ft-item-company-social">
-                        <a href="http://facebook.com"><img src={facebook} alt='facebook logo' /></a>
-                        <a href="https://instagram.com"><img src={instagram} alt='instagram logo' /></a>
-                        <a href="https://twitter.com"><img src={twitter} alt='twitter logo' /></a>
-                    </div>
-                </div>
+            <div class="ft-box1">
+                <h2>SaveFood</h2>
+                <p class="footer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, metus id fermentum ultrices, quam urna luctus felis, quis malesuada lorem ex ut metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum, metus id fermentum ultrices, quam urna luctus felis, quis malesuada lorem ex ut metus.</p>
+                <a href="http://facebook.com"><img src={facebook} alt='facebook logo' /></a>
+                <a href="https://instagram.com"><img src={instagram} alt='instagram logo' /></a>
+                <a href="https://twitter.com"><img src={twitter} alt='twitter logo' /></a>
+                
+            </div>
 
-                <div class="ft-item-links">
-                    <h3>Sitemap</h3>
-                    <ul>
-                        <li><a href="/index" alt="home">Home</a></li>
-                        <li>About us</li>
-                        <li>Register your restaurant</li>
-                    </ul>
-                </div>
-
-                <div class="ft-item-links">
-                    <h3>More information</h3>
-                    <ul>
-                        <li>Look all cities</li>
-                        <li>Read more in our FAQ</li>
-                        <li>How you can help me us</li>
-                    </ul>
-                </div>
+            <div class="ft-box2">
+                <h3>Sitemap</h3>
+                <ul>
+                    <li><a href="/index" alt="home">Home</a></li>
+                    <li>About us</li>
+                    <li>Register your restaurant</li>
+                </ul>
+            </div>
+            
+            <div class="ft-box2">
+            <h3>More information</h3>
+                <ul>
+                    <li>Look all cities</li>
+                    <li>Read more in our FAQ</li>
+                    <li>How you can help me us</li>
+                </ul>
             </div>
         </footer>
     );
