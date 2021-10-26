4883c452e3f38bcc1762ae452a0447cb77589639
Bárbara Nassar <ba.almeida@hotmail.com>
2021-10-26 21:30:39 +0200

    #9: Fix links in react

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,6 @@
 import { Divide } from 'hamburger-react';
 import './footer.css';
+import { Link } from 'react-router-dom';
 import facebook from '../../assets/facebook.png'
 import instagram from '../../assets/instagram.png'
 import twitter from '../../assets/twitter.png'
@@ -19,18 +20,18 @@ function Footer() {
             <div className="ft-box2">
                 <h3>Sitemap</h3>
                 <ul>
-                    <li><a href="/index" alt="home">Home</a></li>
-                    <li>About us</li>
-                    <li>Register your restaurant</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About us</Link></li>
+                    <li><Link to="/restaurant">Register your restaurant</Link></li>
                 </ul>
             </div>
             
             <div className="ft-box2">
             <h3>More information</h3>
                 <ul>
-                    <li>Look all cities</li>
-                    <li>Read more in our FAQ</li>
-                    <li>How you can help me us</li>
+                    <li><Link to="/about">Look all cities</Link></li>
+                    <li><Link to="/faq">Read more in our FAQ</Link></li>
+                    <li><Link to="/help-us">How you can help me us</Link></li>
                 </ul>
             </div>
         </footer>
