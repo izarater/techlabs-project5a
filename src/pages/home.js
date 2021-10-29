647f7e38bcc81860481ba6544708f984da5ed161
jparrar <jparrar@unal.edu.co>
2021-10-28 19:14:23 -0500

    add infopills component to homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import Header from '../components/header/header';
 import Main from '../components/main/main';
+import InfoPills from '../components/infopills/infopills';
 import Footer from '../components/footer/footer';
 
 function Home() {
@@ -7,6 +8,7 @@ function Home() {
     <>
       <Header />
       <Main />
+      <InfoPills />
       <Footer />
     </>
   );
