6299e3f76ba3f673ed43b453aa97441e14429724
Bárbara Nassar <ba.almeida@hotmail.com>
2021-10-30 04:23:55 +0200

    #6: Create restaurant section and add in the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,14 @@
 import Header from '../components/header/header';
 import Main from '../components/main/main';
 import Footer from '../components/footer/footer';
+import Restaurant from '../components/restaurant/restaurant';
 
 function Home() {
   return (
     <>
       <Header />
       <Main />
+      <Restaurant />
       <Footer />
     </>
   );
