601a39807718e6a1e28461ed5639e837a16a3bcc
Bárbara Nassar <ba.almeida@hotmail.com>
2021-10-30 23:36:46 +0200

    #16: Insert a button in the main section

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,9 +1,16 @@
 import './main.css';
+import { Link } from 'react-router-dom';
+import Button from '../button/button';
 
 function Main() {
   return (
     <main className="main">
       <h1 className="title">Let's save food</h1>
+      <div className="main-button">
+        <Link to="/discovery">
+          <Button variant="quaternary">Look for food!</Button>
+        </Link>  
+      </div>
       <p className="description">
         Do you feel in need for a hand? Maybe getting food for free or 
         at lower price than usual? Start to use SaveFood right now and 
