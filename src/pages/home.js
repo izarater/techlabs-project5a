a02e958435565247dfde0ea8ba532e920d3a7171
jparrar <jparrar@unal.edu.co>
2021-10-30 18:47:02 -0500

    this is stats section. The buttons have troubles with position both in desktop and mobile version.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,8 @@
 import Header from '../components/header/header';
 import Main from '../components/main/main';
 import InfoPills from '../components/infopills/infopills';
+import Stats from '../components/stats/stats';
+import WhatIs from '../components/whatis/whatis';
 import Footer from '../components/footer/footer';
 
 function Home() {
@@ -8,7 +10,9 @@ function Home() {
     <>
       <Header />
       <Main />
+      <WhatIs />
       <InfoPills />
+      <Stats />
       <Footer />
     </>
   );
