9919ba58d3ebf839960e82ac5221d2e30fa31eae
jparrar <jparrar@unal.edu.co>
2021-10-30 17:44:14 -0500

    class on the divs was changed to className

--- src/components/infopills/infopills.js
+++ src/components/infopills/infopills.js
@@ -4,21 +4,20 @@ function InfoPills () {
     
     return (
         
-        <div class="infoPills">
+        <div className="infoPills">
             <h1>Important information</h1>
 
-            <div class="subInfoPills">
-                <div class="infoPill">
-                    <h3 class="subtitleInfoPills">35% of the wasted food</h3>
+            <div className="subInfoPills">
+                <div className="infoPill">
+                    <h3 className="subtitleInfoPills">35% of the wasted food</h3>
                     <p>Huge amounts of food is also lost simply because we throw it away. They are out by supermarkets, shops, and households. Much of it is still perfectly fit for eating.</p>
                 </div>
-                
-                <div class="infoPill">
-                    <h3 class="subtitleInfoPills">Environmental problems</h3>
+                <div className="infoPill">
+                    <h3 className="subtitleInfoPills">Environmental problems</h3>
                     <p>Food production requires energy, pesticides, land and so on. Besides, food production contributes to the emission of greenhouse gasses.</p>
                 </div>
-                <div class="infoPill">
-                    <h3 class="subtitleInfoPills">Food waste and climate change</h3>
+                <div className="infoPill">
+                    <h3 className="subtitleInfoPills">Food waste and climate change</h3>
                     <p>The carbon footprint of food waste is estimated to be 3.3 billion tons of CO2 equivalents released into the atmosphere a year.</p>
                 </div>
             </div>
