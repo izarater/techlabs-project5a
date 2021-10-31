bc563cf412c543bf04684bca799f098d4983d3e6
Nar <51009725+narvmtz@users.noreply.github.com>
2021-10-30 19:31:51 -0500

    minor changes in whatis component style

--- src/components/whatis/whatis.js
+++ src/components/whatis/whatis.js
@@ -1,22 +1,26 @@
-import './whatis.css'
-import Button from '../button/button'
+import './whatis.css';
+import Button from '../button/button';
 
-function WhatIs () {
-    return (
-        <div className='whatIsContent'>
-            <h1 id='whatIsTitle'>What is SaveFood?</h1>
-            <p id='whatIsText'>
-                We connect restaurants or stores which usually throw food away
-                with people who may have a difficult situation so they can get a hand.
-                Our goal is to prevent restaurants or other stablishments from wasting
-                more food by giving it to those in need. 
-            </p>
-            <div className='whatIsButtons'>
-                <Button variant='primary'>Read More</Button>
-                <Button  variant='tertiary'>Start Using</Button>
-            </div>
-        </div>
-    )
+function WhatIs() {
+  return (
+    <div className="whatIsContent">
+      <h1 id="whatIsTitle">What is SaveFood?</h1>
+      <p id="whatIsText">
+        We connect restaurants or stores which usually throw food away with
+        people who may have a difficult situation so they can get a hand. Our
+        goal is to prevent restaurants or other stablishments from wasting more
+        food by giving it to those in need.
+      </p>
+      <div className="whatIsButtons">
+        <Button variant="primary" size="m">
+          Read More
+        </Button>
+        <Button variant="tertiary" size="m">
+          Start Using
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default WhatIs
+export default WhatIs;
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,14 @@ import Header from '../components/header/header';
 import Main from '../components/main/main';
 import InfoPills from '../components/infopills/infopills';
 import Footer from '../components/footer/footer';
+import WhatIs from '../components/whatis/whatis';
 
 function Home() {
   return (
     <>
       <Header />
       <Main />
+      <WhatIs />
       <InfoPills />
       <Footer />
     </>
