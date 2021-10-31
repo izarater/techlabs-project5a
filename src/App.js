b81f134f8df848122bdaa28958d57e293317baaf
Samir <szarater@correo.udistrital.edu.co>
2021-10-31 09:34:46 -0500

    implementing login page and some changes on register

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Home from './pages/home';
 import Discovery from './pages/discovery';
 import RegisterPage from './pages/registerPage';
+import LoginPage from './pages/loginPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/discovery" component={Discovery} />
         <Route path="/signup" component={RegisterPage} />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </Router>
   );
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,9 +1,9 @@
-import './register.css';
+import './login.css';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Button from '../button/button';
 
-function Register() {
+function Login() {
   const { register, handleSubmit, errors } = useForm();
   const [userInfo, setUserInfo] = useState();
   const onSubmit = (data) => {
@@ -12,35 +12,14 @@ function Register() {
   };
   console.log(errors)
   return (
-    <div className='register'>
+    <div className='login'>
     <div className='container'>
       <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
       <form onSubmit={ handleSubmit(onSubmit)}>
-        <h1 className="regtitle">Registration Form</h1>
+        <h1 className="regtitle">Login</h1>
         <div className='ui divider'></div>
         <div className='ui form'>
-          <br/>
-          <div className='field'>
-            <label>Name</label>
-            <br/>
-            <input 
-              type='text' 
-              name='name' 
-              //placeholder='Name' 
-              {...register("A", {required:'Name is required'})}/>
-            </div>
-
-          <br/>
-          <div className='field'>
-            <label>Surname</label>
-            <br/>
-            <input 
-              type='text' 
-              name='surname' 
-              //placeholder='Surname' 
-              {...register("B", {required:'Surname is required'})}/>
-          </div>   
-
+          
           <br/>
           <div className='field'>
             <label>Username</label>
@@ -49,21 +28,8 @@ function Register() {
               type='text' 
               name='username' 
               //placeholder='Username' 
-              {...register("C", {required:'Username is required'})}/>
+              {...register("A", {required:'Username is required'})}/>
           </div>
-          
-
-          <br/>
-          <div className='field'>
-            <label>Email</label>
-            <br/>
-            <input 
-              type='email' 
-              name='email' 
-              //placeholder='Email' 
-              {...register("D", {required:'Email is required', pattern:{value:/^\S+@\S+$/i,message:'This is not valid email'}})}/>
-          </div>
-          
 
           <br/>
           <div className='field'>
@@ -73,7 +39,7 @@ function Register() {
               type='password' 
               name='password' 
               //placeholder='Password' 
-              {...register("E", {
+              {...register("B", {
                 required:'Password is required', 
                 minLength: {
                   value:4, 
@@ -97,7 +63,7 @@ function Register() {
 );
 }
 
-export default Register;
+export default Login;
 
 // <p>{errors.username?.message}</p>
 // <p>{errors.email?.message}</p>
