d6e5c9efb795ea9da6aeb31039efb9ea543d68f1
Nar <51009725+narvmtz@users.noreply.github.com>
2021-10-31 12:28:16 -0500

    Fix list component to consume the info from json file

--- src/components/foodItem/foodItem.js
+++ src/components/foodItem/foodItem.js
@@ -1,26 +1,23 @@
 import './foodItem.css';
-import Button from '../button/button'
+import Button from '../button/button';
 
-function FoodItem (props) {
-    return (
-        <div className='itemContainer'>
-            <img
-                id='itemImage'
-                src={props.imageSrc}
-                alt={props.imageAlt}
-            ></img>
-            <h2>
-                {props.itemName}
-            </h2>
-            <p id='itemDescription'>
-                Quantity: {props.quantity}<br/>
-                Description: {props.foodDescription}
-            </p>
-            <div id='reserveButton'>
-                <Button variant='primary' size='m'>Reserve</Button>
-            </div>
-        </div>
-    )
+function FoodItem(props) {
+  return (
+    <div className="itemContainer">
+      <img src={props.imageSrc} alt={props.imageAlt} />
+      <div className="itemInfo">
+        <h2>{props.itemName}</h2>
+        <p id="itemDescription">
+          Quantity: {props.quantity}
+          <br />
+          Description: {props.foodDescription}
+        </p>
+        <Button variant="primary" size="m">
+          Reserve
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default FoodItem
+export default FoodItem;
---
+++ src/components/foodList/dataFood.js
@@ -0,0 +1,90 @@
+export const dataFood = [
+  {
+    id: 1,
+    name: 'Pizza Planet',
+    quantity: '2-4 people.',
+    description:
+      'The old classic italian favorite. Perfect for any kind of situation.',
+    src: 'https://tvpacifico.mx/recetas/intranet/images/recipes/330-349.jpg',
+    alt: 'a pizza',
+    tags: ['pizza'],
+  },
+  {
+    id: 2,
+    name: 'burger Monster',
+    quantity: '1',
+    description: 'burger, fries and sauces. Nothing more american than that!',
+    src: 'https://i.ibb.co/1dDXHMS/Image-2.png',
+    alt: 'A burger, fries and sauces',
+    tags: ['burger'],
+  },
+  {
+    id: 3,
+    name: 'EatWell',
+    quantity: '1',
+    description:
+      'Avocado salad, toasted bread and fried egg. A healthy and tasty way to begin the day.',
+    src: 'https://i.ibb.co/VMTS5ck/Image-3.png',
+    alt: 'avocado toasted bread and fraid egg',
+    tags: ['pizza'],
+  },
+  {
+    id: 4,
+    name: 'Tutti Frutti  ',
+    quantity: '2-4 people',
+    description:
+      'Kiwi, strawberries, apple, pear, papaya... All those flavors at once in your mouth!',
+    src: 'https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2021/07/ensalada-de-frutas-de-temporada-para-el-desayuno.jpg',
+    alt: 'fruit salad',
+    tags: ['desert'],
+  },
+  {
+    id: 5,
+    name: 'Golden Rice',
+    quantity: '3-5 people',
+    description:
+      'Titanic shrimps on the best asian rice you will ever try. More than enough to feed an entire family!',
+    src: 'https://www.restaurantedragonoriental.com/images/cabecero-chowfan2.jpg',
+    alt: 'avocado toasted bread and fraid egg',
+    tags: ['pizza'],
+  },
+  {
+    id: 6,
+    name: 'Burrito Madness',
+    quantity: '2 people',
+    description:
+      'Because hamburgers are not the only way a meal can taste good when you eat it all at once on each bite. Meat, vegetables, cheese and love.',
+    src: 'https://d1kxxrc2vqy8oa.cloudfront.net/wp-content/uploads/2020/01/09213530/RFB-2312-1-burrito.jpg',
+    alt: 'avocado toasted bread and fraid egg',
+    tags: ['mexican'],
+  },
+  {
+    id: 7,
+    name: 'Burger Monster',
+    quantity: '1',
+    description: 'Burger, fries and sauces. Nothing more american than that!',
+    src: 'https://i.ibb.co/1dDXHMS/Image-2.png',
+    alt: 'A burger, fries and sauces',
+    tags: ['burger'],
+  },
+  {
+    id: 8,
+    name: 'EatWell',
+    quantity: '1',
+    description:
+      'Avocado salad, toasted bread and fried egg. A healthy and tasty way to begin the day.',
+    src: 'https://i.ibb.co/VMTS5ck/Image-3.png',
+    alt: 'avocado toasted bread and fraid egg',
+    tags: ['pizza'],
+  },
+  {
+    id: 9,
+    name: 'Pizza Planet',
+    quantity: '2-4 people.',
+    description:
+      'The old classic italian favorite. Perfect for any kind of situation.',
+    src: 'https://tvpacifico.mx/recetas/intranet/images/recipes/330-349.jpg',
+    alt: 'a pizza',
+    tags: ['pizza'],
+  },
+];
