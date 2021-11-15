26d78a943879c3be606e3b1c263588b6aab95d86
Isaac Zarate <izarater@unal.edu.co>
2021-11-15 15:56:06 -0500

    connecting frontend and backend with Axios on restaurants page (discovery)

--- src/pages/discovery.js
+++ src/pages/discovery.js
@@ -1,11 +1,13 @@
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
 import ContextFilter from '../context/filter.context';
+import ResList from '../components/reslist/reslist';
 
 function Discovery() {
   return (
     <>
       <Header />
+      <ResList />
       <ContextFilter />
       <Footer />
     </>
